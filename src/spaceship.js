8d34fcc23acbed1fe9e949ce30d1561550c20f8f
Sean Altemose <sean@seanaltemose.com>
2019-07-09 21:59:55 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -11,4 +11,7 @@ class Spaceship {
     crew.length ? (this.docked = false) : (this.docked = true);
     this.notifyCrew();
   }
+  notifyCrew() {
+    this.crew.forEach(crewMember => (crewMember.currentShip = this))
+  }
 }
